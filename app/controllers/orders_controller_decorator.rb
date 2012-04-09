a5b65453668f509e422a6e8aff05fa22b8b2112d
Fedorenko Dmitriy <c0va23@gmail.com>
2012-04-09 10:56:07 +0400

    Render 404 for /order/:id/sberbank_invoice when order.payment_method is not SberBankInvoice

--- app/controllers/orders_controller_decorator.rb
+++ app/controllers/orders_controller_decorator.rb
@@ -1,6 +1,9 @@
 OrdersController.class_eval do
   def sberbank_invoice
     @order = Order.find_by_number!(params[:id])
+    unless @order.payment_method.is_a?(PaymentMethod::SberBankInvoice)
+      render_404 and return
+    end
     @preferences = @order.payment_method.preferences
     render :layout => false
   end
