812af391120655e08879e2823e59e0c1d38bb123
Fedorenko Dmitriy <c0va23@gmail.com>
2012-04-10 15:24:03 +0400

    Fixed helper "number_to_currency_with_synergy"

--- lib/ext/number_helper.rb
+++ lib/ext/number_helper.rb
@@ -2,7 +2,7 @@
 
 ActionView::Helpers::NumberHelper.module_eval do
   def number_to_currency_with_synergy(number, options = {})
-    number_to_currency_without_synergy(number, options).gsub(' ', "\302\240")
+    number_to_currency_without_synergy(number, options).try(:gsub, ' ', "\302\240")
   end
   alias_method_chain :number_to_currency, :synergy
 end
