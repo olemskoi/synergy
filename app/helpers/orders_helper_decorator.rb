4e56613e9513b4a231d553318e2660ca882680e1
Sergey Rezvanov <sergey@rezvanov.info>
2012-05-17 15:12:55 +0600

    Fixed sberbank invoice address line

---
+++ app/helpers/orders_helper_decorator.rb
@@ -0,0 +1,10 @@
+OrdersHelper.module_eval do
+  def sberbank_address_line(address)
+    arr, zipcode, city = [], address.zipcode, address.city
+    address12 = [ address.address1, address.address2 ].join
+
+    [ zipcode, city, address12 ].each{ |i| arr << i unless i.present? }
+
+    arr.join(', ')
+  end
+end
