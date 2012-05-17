ac1201a2dff624b33f460bb5ec658d2b04cf384f
Sergey Rezvanov <sergey@rezvanov.info>
2012-05-17 16:58:19 +0600

    Fixed bug with condition

--- app/helpers/orders_helper_decorator.rb
+++ app/helpers/orders_helper_decorator.rb
@@ -3,7 +3,7 @@ OrdersHelper.module_eval do
     arr, zipcode, city = [], address.zipcode, address.city
     address12 = [ address.address1, address.address2 ].join
 
-    [ zipcode, city, address12 ].each{ |i| arr << i unless i.present? }
+    [ zipcode, city, address12 ].each{ |i| arr << i if i.present? }
 
     arr.join(', ')
   end
