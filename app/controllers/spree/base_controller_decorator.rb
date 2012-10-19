b6c750deb8eb16cb2b9b00938bc21d000f306108
Sergey Rezvanov <sergey@rezvanov.info>
2012-10-19 13:06:34 +0600

    Rewritten then method render_404 with formats

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -1,6 +1,10 @@
 Spree::BaseController.class_eval do       
   def render_404(exception = nil)
-    @title = I18n.t(:page_not_found)
-    render :template => 'errors/404', :status => "404 Not Found"
+    respond_to do |format|
+      format.html do
+        render :status => :not_found, :template => 'errors/404'
+      end
+      format.all { render :status => :not_found, :nothing => true }
+    end
   end
 end
