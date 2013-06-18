043912803f1a909382eb081a89b83d60340e0a0a
kanfet <byte.developer@gmail.com>
2013-06-18 15:02:19 +0600

    fix "NoMethodError occurred in checkout#update: undefined method 'calculator' for nil:NilClass"

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -2,7 +2,7 @@ CheckoutController.class_eval do
   private
 
   def after_payment
-    if @order.shipping_method.calculator.is_a?(Calculator::CashOnDelivery)
+    if @order.shipping_method && @order.shipping_method.calculator.is_a?(Calculator::CashOnDelivery)
       @order.payments.build(:payment_method => PaymentMethod.internal, :amount => @order.total)
       @order.confirm_without_payment!
     end
